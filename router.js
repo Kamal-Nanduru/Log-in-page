0274ee98bef978876c0943d7bea975b3624ee8bf
Kamal-Nanduru <kamalnanduruitqueries@gmail.com>
2023-06-07 10:47:24 +0530

    final edit

--- router.js
+++ router.js
@@ -1,19 +1,41 @@
 var express = require("express");
 var router = express.Router();
 
-const credential = {
-    email: "kamal@gmail.com",
-    password: "admin123"
+const  credential = {
+    email : "kamal@gmail.com",
+    password : "kamal123"
 }
 
-//login user
-router.post('/login', (req,res) => {
+// login user
+router.post('/login', (req, res)=>{
     if(req.body.email == credential.email && req.body.password == credential.password){
-     req.session.user = req.body.email;
-     res.redirect('/dashboard');
+        req.session.user = req.body.email;
+        res.redirect('/route/dashboard');
+        //res.end("Login Successful...!");
     }else{
-      res.end("Invalid Credentials")
+        res.end("Invalid Username")
     }
 });
+
+// route for dashboard
+router.get('/dashboard', (req, res) => {
+    if(req.session.user){
+        res.render('dashboard', {user : req.session.user})
+    }else{
+        res.send("Unauthorize User")
+    }
+})
+
+// route for logout
+router.get('/logout', (req ,res)=>{
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err);
+            res.send("Error")
+        }else{
+            res.render('base', { title: "Express", logout : "logout Successfully...!"})
+        }
+    })
+})
 
 module.exports = router;
